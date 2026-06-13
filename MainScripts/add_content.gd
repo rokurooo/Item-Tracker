ca1aa29602d5ea7b7021006e119d30592962a11a
rakehoe <ricojohn.casquete@wvsu.edu.ph>
2026-06-13 19:35:57 +0800

    UI and button functions on adding products

---
+++ MainScripts/add_content.gd
@@ -0,0 +1,29 @@
+@tool
+extends Button
+
+var header: PanelContainer
+var menu_toggled: bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Menu.visible = false
+	header = get_parent().get_parent().get_parent().get_parent()
+	pass # Replace with function body.
+
+func _process(_delta: float) -> void:
+	if header and !header.SHOWMENU:
+		$Menu.visible = false
+	else:
+		$Menu.visible = true
+
+
+func _on_pressed() -> void:
+	if !menu_toggled:
+		header.SHOWMENU = true
+		$Menu.visible = true
+		menu_toggled = true
+	else:
+		header.SHOWMENU = false
+		$Menu.visible = false
+		menu_toggled = false
+
+	pass # Replace with function body.
--- MainScripts/label_holder.gd
+++ MainScripts/label_holder.gd
@@ -1,11 +1,29 @@
+@tool
 extends VBoxContainer
 
+@export var edit_toggled: bool = false
 var options_toggled: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$EditMenu.visible = false
 	pass # Replace with function body.
 	
+func _process(_delta: float) -> void:
+	if edit_toggled:
+		$EditMenu.visible = true
+	else:
+		$EditMenu.visible = false
+
+func _on_edit_pressed() -> void:
+	if !edit_toggled:
+		edit_toggled = true
+		$EditMenu.visible = true
+	else:
+		edit_toggled = false
+		$EditMenu.visible = false
+	pass # Replace with function body.
+
 func _on_delete_pressed() -> void:
 	queue_free()
 	pass # Replace with function body.
@@ -18,3 +36,4 @@ func _on_labels_pressed() -> void:
 		$TogglePlayer.play_backwards("Open")
 		options_toggled = false
 	pass # Replace with function body.
+
